2cb085df67816de204a26984cc5dfa6d2ee630a3
zopaj63 <zoran.pajnic@gmail.com>
2023-09-30 21:43:04 +0200

    welcome blade error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,10 @@
 <h1>Welcome blade</h1>
 <p>Uspješna/neuspješna registracija o čemu svjedoče poruke ispod:</p>
 
-@if(session("succes"))
-    {{session("succes")}}
+@if(session("success"))
+    {{session("success")}}
 @elseif(session("error"))
     {{session("error")}}
 @endif
 
-<h3>Registrirani korisnici</h3>
-<ol>
-    @foreach($korisniks as $korisnik)
-    <li>
-        {{$korisnik->name}}
-        {{$korisnik->email}}
-        <hr>
-    </li>
-    @endforeach
-</ol>
 
